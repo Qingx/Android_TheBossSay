02ab1b2118d18b07573a063567d35f302c695f4b
may <cymbidium@outlook.com>
2021-11-01 17:28:26 +0800

    一键授权登录 完成

--- app/src/main/java/net/cd1369/tbs/android/ui/fragment/HomeMineFragment.kt
+++ app/src/main/java/net/cd1369/tbs/android/ui/fragment/HomeMineFragment.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.ListView
 import androidx.recyclerview.widget.LinearLayoutManager
 import cn.jiguang.verifysdk.api.JVerificationInterface
 import cn.jiguang.verifysdk.api.LoginSettings
